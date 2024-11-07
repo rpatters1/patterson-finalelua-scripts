a9f67852763c6c0c768a672d7c7ad175c55ae93f
Robert Patterson <robert@robertgpatterson.com>
2024-11-07 14:19:39 -0600

    add status window

--- src/move_exports.lua
+++ src/move_exports.lua
@@ -5,11 +5,13 @@ function plugindef()
     finaleplugin.ExecuteExternalCode = true
     finaleplugin.CategoryTags = "Document"
     finaleplugin.MinJWLuaVersion = 0.76
-    return "Move Exports...", "Move Exports", "Move all exports to a folder called ‘-exports’"
+    return "Move Exports...", "Move Exports", "Copy Finale docs to Finale 27 versions and move them and all exports to a folder called ‘-exports’"
 end
 
 local utils = require("library.utils")
 local client = require("library.client")
+local mixin = require("library.mixin")
+
 local lfs = require("lfs")
 
 local SUBFOLDER_NAME <const> = "-exports"
@@ -18,6 +20,8 @@ local MUSESTYLE_EXTENSION <const> = ".mss"
 local ENIGMAXML_EXTENSION <const> = ".enigmaxml"
 local MUSX_EXTENSION <const> = ".musx"
 local MUS_EXTENSION <const> = ".mus"
+
+local TIMER_ID <const> = 1
 
 local function select_directory()
     local default_folder_path = finale.FCString()
@@ -71,9 +75,6 @@ local function move_to_export_folder(path, filename)
     return success, msg_or_status
 end
 
-local FOLDER_SEP <const> = finenv.UI():IsOnMac() and "/" or "\\"
-local SUBFOLDER_STRING <const> = FOLDER_SEP .. SUBFOLDER_NAME .. FOLDER_SEP
-
 local function export_updated_document(path, file, extension)
     local calc_mod_time = function(utf8_path)
         local attr = lfs.attributes(client.encode_with_client_codepage(utf8_path))
@@ -115,18 +116,92 @@ local function export_updated_document(path, file, extension)
     end
 end
 
+local function process_document(path, file, extension)
+    if extension == MUSICXML_EXTENSION or extension == MUSESTYLE_EXTENSION or extension == ENIGMAXML_EXTENSION then
+        move_to_export_folder(path, file .. extension)
+    elseif extension == MUS_EXTENSION then
+        local attr = lfs.attributes(client.encode_with_client_codepage(path .. file .. MUSX_EXTENSION))
+        if not attr or attr.mode ~= "file" then
+            export_updated_document(path, file, extension)
+        end
+    elseif extension == MUSX_EXTENSION then
+        export_updated_document(path, file, extension)
+    end
+end
+
+function run_status_dialog(selected_directory, files_to_process)
+    local dialog = mixin.FCXCustomLuaWindow()
+        :SetTitle("Move Exports to Exports Folder")
+    local current_y = 0
+    -- processing folder
+    dialog:CreateStatic(0, current_y + 2, "folder_label")
+        :SetText("Folder:")
+        :DoAutoResizeWidth(0)
+    dialog:CreateStatic(0, current_y + 2, "folder")
+        :SetText("")
+        :SetWidth(400)
+        :AssureNoHorizontalOverlap(dialog:GetControl("folder_label"), 5)
+        :StretchToAlignWithRight()
+    current_y = current_y + 20
+    -- processing file
+    dialog:CreateStatic(0, current_y + 2, "file_path_label")
+        :SetText("File:")
+        :DoAutoResizeWidth(0)
+    dialog:CreateStatic(0, current_y + 2, "file_path")
+        :SetText("")
+        :SetWidth(300)
+        :AssureNoHorizontalOverlap(dialog:GetControl("file_path_label"), 5)
+        :HorizontallyAlignLeftWith(dialog:GetControl("folder"))
+        :StretchToAlignWithRight()
+    -- cancel
+    dialog:CreateCancelButton("cancel")
+    -- registrations
+    dialog:RegisterInitWindow(function(self)
+        self:SetTimer(TIMER_ID, 100) -- 100 milliseconds
+    end)
+    dialog:RegisterHandleTimer(function(self, timer)
+        assert(timer == TIMER_ID, "incorrect timer id value " .. timer)
+        if #files_to_process <= 0 then
+            self:StopTimer(TIMER_ID)
+            self:GetControl("folder"):SetText(selected_directory)
+            self:GetControl("file_path"):SetText("(export complete)")
+            self:GetControl("cancel"):SetText("Close")
+            return
+        end
+        self:GetControl("folder"):SetText("..." .. files_to_process[1].path:sub(#selected_directory))
+            :RedrawImmediate()
+        self:GetControl("file_path"):SetText(files_to_process[1].file .. files_to_process[1].extension)
+            :RedrawImmediate()
+        process_document(files_to_process[1].path, files_to_process[1].file, files_to_process[1].extension)
+        table.remove(files_to_process, 1)
+    end)
+    dialog:RegisterCloseWindow(function(self)
+        self:StopTimer(TIMER_ID)
+        if #files_to_process > 0 then
+            print("processing aborted by user")
+        end
+        finenv.RetainLuaState = false
+    end)
+    dialog:RunModeless()
+end
+
+local FOLDER_SEP <const> = finenv.UI():IsOnMac() and "/" or "\\"
+local SUBFOLDER_STRING <const> = FOLDER_SEP .. SUBFOLDER_NAME .. FOLDER_SEP
+
+local files_to_process = {}
+
 for path, filename in utils.eachfile(selected_folder, true) do
     if path:sub(-SUBFOLDER_STRING:len()) ~= SUBFOLDER_STRING then
         local _, file, extension = utils.split_file_path(filename)
-        if extension == MUSICXML_EXTENSION or extension == MUSESTYLE_EXTENSION or extension == ENIGMAXML_EXTENSION then
-            move_to_export_folder(path, filename)
-        elseif extension == MUS_EXTENSION then
-            local attr = lfs.attributes(client.encode_with_client_codepage(path .. file .. MUSX_EXTENSION))
-            if not attr or attr.mode ~= "file" then
-                export_updated_document(path, file, extension)
-            end
-        elseif extension == MUS_EXTENSION then
-            export_updated_document(path, file, extension)
+        if extension == MUSX_EXTENSION or extension == MUS_EXTENSION or extension == ENIGMAXML_EXTENSION
+                    or extension == MUSICXML_EXTENSION or extension == MUSESTYLE_EXTENSION then
+            table.insert(files_to_process, {file = file, path = path, extension = extension})
         end
     end
+end
+
+if #files_to_process > 0 then
+    run_status_dialog(selected_folder, files_to_process)
+else
+    finenv.UI():AlertInfo("No exported files found in " .. selected_folder, "")
 end
